0b88e63f1d4a78cd3e5daaed10e5daef8c575797
George <gnm7208@gmail.com>
2025-11-16 19:53:09 +0300

    Implement Song class with attributes and initialization logic

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,16 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.artists.append(artist)
+        Song.genres.append(genre)
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
